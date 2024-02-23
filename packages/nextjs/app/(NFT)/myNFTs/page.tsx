e6787d6f29bfae23ae4bb0768b8832ff4ffd6edc
saremeskandary <saremeskandary@gmail.com>
2024-02-23 21:49:15 +0330

    configed by new contract

---
+++ packages/nextjs/app/(NFT)/myNFTs/page.tsx
@@ -0,0 +1,74 @@
+import { MyHoldings } from "./_components";
+import type { NextPage } from "next";
+import { useAccount } from "wagmi";
+import { MetaHeader } from "~~/components/MetaHeader";
+import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { notification } from "~~/utils/scaffold-eth";
+import { ipfsClient } from "~~/utils/simpleNFT";
+import nftsMetadata from "~~/utils/simpleNFT/nftsMetadata";
+
+const MyNFTs: NextPage = () => {
+  const { address: connectedAddress, isConnected, isConnecting } = useAccount();
+
+  const { writeAsync: safeMint } = useScaffoldContractWrite({
+    contractName: "YourCollectible",
+    functionName: "safeMint",
+    args: [connectedAddress, ""],
+  });
+
+  const { data: tokenIdCounter } = useScaffoldContractRead({
+    contractName: "YourCollectible",
+    functionName: "tokenIdCounter",
+    watch: true,
+    cacheOnBlock: true,
+  });
+
+  const handlesafeMint = async () => {
+    // circle back to the zero item if we've reached the end of the array
+    if (tokenIdCounter === undefined) return;
+
+    const tokenIdCounterNumber = parseInt(tokenIdCounter.toString());
+    const currentTokenMetaData = nftsMetadata[tokenIdCounterNumber % nftsMetadata.length];
+    const notificationId = notification.loading("Uploading to IPFS");
+    try {
+      const uploadedItem = await ipfsClient.add(JSON.stringify(currentTokenMetaData));
+
+      // First remove previous loading notification and then show success notification
+      notification.remove(notificationId);
+      notification.success("Metadata uploaded to IPFS");
+
+      await safeMint({
+        args: [connectedAddress, uploadedItem.path],
+      });
+    } catch (error) {
+      notification.remove(notificationId);
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <MetaHeader />
+      <div className="flex items-center flex-col pt-10">
+        <div className="px-5">
+          <h1 className="text-center mb-8">
+            <span className="block text-4xl font-bold">My NFTs</span>
+          </h1>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        {!isConnected || isConnecting ? (
+          <RainbowKitCustomConnectButton />
+        ) : (
+          <button className="btn btn-secondary" onClick={handlesafeMint}>
+            Mint NFT
+          </button>
+        )}
+      </div>
+      <MyHoldings />
+    </>
+  );
+};
+
+export default MyNFTs;
